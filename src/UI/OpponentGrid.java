86c5e0a96934d36f65694ee8a2c7723040b583c2
Mary <mmelni4@uic.edu>
2017-11-09 13:13:26 -0600

    class opponentGrid and status (enum)

---
+++ src/UI/OpponentGrid.java
@@ -0,0 +1,59 @@
+package UI;
+
+import java.awt.GridLayout;
+import javax.swing.JPanel;
+
+public class OpponentGrid
+{
+	private static Cell grid[][];
+	private static JPanel gridPanel;
+	
+	public OpponentGrid()
+	{
+		gridPanel = new JPanel();
+		gridPanel.setLayout(new GridLayout(10,10,1,1));
+		grid = new Cell[10][10];
+		for (int i = 0; i < 10; i++)
+			for (int j = 0; j < 10; j++)
+			{
+				grid[i][j] = new Cell(i, j, 0);
+				if (grid[i][j].getImage() == null)
+					return;
+				
+				gridPanel.add(grid[i][j].getImage());
+			}
+		Battleship.setOpponentInfo("Opponent Board setup complete");
+	}
+	
+	public static Cell get(int x, int y)
+	{
+		if (x < 0 || x > 9 
+			|| y < 0 || y > 9)
+			return null;
+		else
+			return grid[x][y];
+	}
+	
+	public JPanel getOppPanel()
+	{
+		return gridPanel;
+	}
+	
+	public void setStatus(int x, int y, Status status)
+	{
+		grid[x][y].setStatus(status);
+	}
+	
+	public static void updateBoard()
+	{
+		gridPanel = new JPanel();
+		gridPanel.setLayout(new GridLayout(3,3,0,0));
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				gridPanel.add(grid[i][j].getImage());				
+			}
+		}
+	}
+}
---
+++ src/UI/Status.java
@@ -0,0 +1,5 @@
+package UI;
+
+public enum Status {
+	EMPTY, UNTOUCHED, HIT, MISS
+}
